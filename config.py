3b90322dfa52bc376c94e7a5ecaa7fc535edab9d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-16 21:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
+API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = getenv("OWNER_ID", 1356469075)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
